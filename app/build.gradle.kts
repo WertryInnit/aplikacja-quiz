f01c67fac42c37a1717af510809b5cd6f8d1780f
WertryInnit <wertryyoung@gmail.com>
2026-09-24 10:13:07 +0200

    dodanie zaleznosci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 }
